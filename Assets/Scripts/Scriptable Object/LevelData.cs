43e5474def673cd1c7266b0825f55128929a2ec0
JeongTaeWoong99 <111197095+JeongTaeWoong99@users.noreply.github.com>
2025-07-19 15:18:07 +0900

    버그 수정
    이동 버그 수정.
    (알맞게 무브노드를 입력을 해도, 알맞게 이동하지 않았던 버그 수정)

--- Assets/Scripts/Scriptable Object/LevelData.cs
+++ Assets/Scripts/Scriptable Object/LevelData.cs
@@ -5,15 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Level Data", menuName = "Scriptable Object/Level Data", order = int.MaxValue)]
 public class LevelData : ScriptableObject
 {
-    public int level;                   // 레벨
-    
-    public AudioClip audioClip;         // 사용 음악
-
-    public int countBeat;               // 생성 및 이동 비트 간격
+    public int       level;      // 레벨
+    public AudioClip audioClip;  // 사용 음악
+    public int       countBeat;  // 생성 및 이동 비트 간격
+    public float     nodeSpeed;  // 비트와 노드가 중앙에 도착하는 시간을 맞추기 위해서 노드 스피드 조절
     
     public List<StringData> stringData; // 스트링 배열 패턴
-
-    public int monsterCount;             // 생성될 몬스터 수
 }
 
 [Serializable]
